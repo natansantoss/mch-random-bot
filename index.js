dfe8e9422b368dfd8a403f8896aa0a690124d60d
Filipe Forattini <filipeforattini1@gmail.com>
2020-04-26 22:24:12 -0300

    feat: logger to catch variables

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 module.exports = async ({ me, myTeam, enemies, lastRound }) => {
   const enemy = enemies.pop()
 
+  console.debug({
+    me,
+    myTeam,
+    enemies,
+    lastRound,
+  })
+
   return {
     target: enemy._id,
     actions: [
