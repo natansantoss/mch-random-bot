39d64896d0fce81144426d30084728a9a8fc8fe8
Natan Santos <63278368+natansantoss@users.noreply.github.com>
2021-07-15 15:08:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const brain = require('./src/dummy')
 
-module.exports = async (t) => {
-  const { me, myTeam, enemies, lastRound } = args
+module.exports = async (args) => {
+  const { me, myTeam, enemies, lastRound  = args
 
   console.debug({
     args,
