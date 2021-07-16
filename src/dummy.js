97e988dcc160b478eb81af19903d7f2966c60d09
Natan Santos <63278368+natansantoss@users.noreply.github.com>
2021-07-16 13:05:40 -0300

    Update dummy.js

--- src/dummy.js
+++ src/dummy.js
@@ -2,7 +2,7 @@ module.exports = ( enemies ) => {
   const enemy = enemies.pop()
   
   return {
-    targer: enemy._id,
+    target: enemy._id,
     actions: [
       { type: 'attack', target: 'head' }, 
       { type: 'attack', target: 'head' }, 
